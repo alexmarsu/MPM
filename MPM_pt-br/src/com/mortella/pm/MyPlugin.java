84ef1af0e98b48c37b41d6f4a2dcff4e69429d6e
Bruno <bmortella@gmail.com>
2013-01-01 15:57:20 -0200

    Update MPM_pt-br/src/com/mortella/pm/MyPlugin.java

--- MPM_pt-br/src/com/mortella/pm/MyPlugin.java
+++ MPM_pt-br/src/com/mortella/pm/MyPlugin.java
@@ -1,7 +1,6 @@
 package com.mortella.pm;
 
 import java.util.logging.Logger;
-
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -12,34 +11,40 @@ import com.mortella.pm.Funcs;
 public class MyPlugin extends JavaPlugin{
 	public final Logger log = Logger.getLogger("Minecraft");
 	public void onEnable(){
-	 log.info("[MPM] v1.0 Plugin Ativado!");	
+	 log.info("[MPM] v1.1 Plugin Ativado!");	
 }
 	
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 	    if(cmd.getName().equalsIgnoreCase("mp")){	    	
-	    
+	     
 	    	if(args.length >= 2){
 	    		if (sender.getServer().getPlayer(args[0]) != null){
 	    		Funcs func = new Funcs();
 	    		String mensagem = func.getMensagem(args);
 	    	  Player alvo = sender.getServer().getPlayer(args[0]);
-	          alvo.sendMessage(ChatColor.YELLOW + "["+ ChatColor.GRAY + sender.getName() + ChatColor.YELLOW + "-MP]" + ChatColor.GREEN + mensagem);
-	          sender.sendMessage(ChatColor.YELLOW + "MP enviada!");
+	         if(sender instanceof Player){
+	        	 alvo.sendMessage(ChatColor.YELLOW + "["+ ChatColor.GRAY + sender.getName() + ChatColor.YELLOW + "-MP]" + ChatColor.GREEN + mensagem);
+	         }else{ 
+	        	 alvo.sendMessage(ChatColor.YELLOW + "["+ ChatColor.MAGIC + sender.getName() + ChatColor.YELLOW + "-MP]" + ChatColor.GREEN + mensagem);
+	         }    	  	          
+	          sender.sendMessage(ChatColor.YELLOW + "MP Enviada!");
 	          log.info("[MPM] O jogador " + sender.getName() + " enviou MP para " + alvo.getDisplayName());
-	          log.info("[MPM] Mensagem:" +  mensagem);
+	          log.info("[MPM] Menssagem:" +  mensagem);
+	        }else{
+	        	sender.sendMessage(ChatColor.RED + "Erro! NÃ£o foi possivel encontrar o jogador.");
 	        }
+	    
 	    }else if(args.length <= 1){
 	    	sender.sendMessage(ChatColor.RED + "Erro:");
 	    	sender.sendMessage(ChatColor.RED + "Use: /mp <jogador> <mensagem>");
 	    	
-	    }else{
-	    	sender.sendMessage(ChatColor.RED + "Erro ao encontrar jogador,talvez ele não esteja online!");
-	    }	      
+	    }
+	    
+	    return true;
 	   
 	}
 	    return true;
 
-	}
-	
+	}	
 }
